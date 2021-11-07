e29667a72644e8cda6340b961d16aeb5a75fd0d8
ribbon-hyupyu <93879611+ribbon-hyupyu@users.noreply.github.com>
2021-11-08 07:24:58 +0900

    Dev

--- main.js
+++ main.js
@@ -74,6 +74,7 @@ function Step1({ goNext }) {
                 <div className="s1-next-btn">다음 단계로</div>
             </div>
             <div className="s1-right">
+                <p className="s1-right-title">오늘 행운 상점의 상품을 순서대로 골라주세요.</p>
                 <div className="s1-products-grid">
                 {step1Products.map(p => (
                     <div key={p.id} className="s1-product" onClick={e => addProduct(e, p)}>
