cf1b1dd427a67cf344325a44a99650b83376ccc5
ribbon-hyupyu <93879611+ribbon-hyupyu@users.noreply.github.com>
2021-11-08 05:09:23 +0900

    Dev

--- main.js
+++ main.js
@@ -7,15 +7,24 @@ function App() {
         setStep(1)
     }
 
+    function goBack(e) {
+        e.preventDefault()
+        if (step !== 1) {
+            setStep(v => v - 1)
+        }
+    }
+
     return (
         <>
             <div className="root-title">
-                <div className="dummy-btn"></div>
+                <div className="title-btn">뒤로</div>
                 <span className="title-text">리본 행운상점 추첨</span>
-                <button className="title-reset-btn" onClick={reset}>초기화</button>
+                <button className="title-btn" onClick={reset}>초기화</button>
             </div>
             <div className="root-content">
-                
+                {step === 1 && <div>1단계</div>}
+                {step === 2 && <div>2단계</div>}
+                {step === 3 && <div>3단계</div>}
             </div>
         </>
     )
